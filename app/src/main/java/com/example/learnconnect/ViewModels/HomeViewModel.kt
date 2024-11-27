977d6c560451ef19afb436ee500ed461bfc762d2
Merve OKTAY <merve.oktay@hotmail.com>
2024-11-27 17:37:43 +0300

    Home Screen Procedures ; UI design and Room database

--- app/src/main/java/com/example/learnconnect/ViewModels/HomeViewModel.kt
+++ app/src/main/java/com/example/learnconnect/ViewModels/HomeViewModel.kt
@@ -3,6 +3,3 @@ package com.example.learnconnect.ViewModels
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
-@HiltViewModel
-class HomeViewModel @Inject constructor() {
-}
